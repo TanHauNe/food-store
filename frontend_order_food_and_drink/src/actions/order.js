35ed89482d4a90c1314bc53be383c43c50a439f5
VoTanHau <hauhauhau22446688@gmail.com>
2023-09-10 22:33:06 +0700

    final

---
+++ frontend_order_food_and_drink/src/actions/order.js
@@ -0,0 +1,38 @@
+export const fetchOrder = async (cartId) =>{
+    
+    const response = await fetch('/api/order', {
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ cartId: cartId })
+    });
+
+    const data = await response.json();
+    return data;
+}
+
+export const fetchPayment = async (cartId) =>{
+    const response = await fetch('/api/payment', {
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ cartId: cartId, bankCode: "" })
+    });
+
+    const data = await response.json();
+    return data;
+}
+
+export const fetchUpdateStatusOrder = async (orderId, accessToken, status)=>{
+    const response = await fetch(`/api/order/status`,{
+        method: 'post',
+        headers: {
+            Authorization: `Bearer ${accessToken}`,
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ orderId: orderId, status: status})
+    });
+    return response;
+}
